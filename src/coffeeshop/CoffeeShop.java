4c0f1a1568d2c4cce5615fce7dc0f67c72602fcf
Konstantinos <kongallis@gmail.com>
2020-11-13 18:40:14 +0200

    Save JSON to ArrayList

--- src/coffeeshop/CoffeeShop.java
+++ src/coffeeshop/CoffeeShop.java
@@ -20,6 +20,45 @@ public class CoffeeShop {
 
     public static void main(String[] args) throws MalformedURLException {
 
+        // Step 1: Getting JSON response into String in Java
+        String response = readJsonFromWebService(CATALOG_ITMES_URL);
+
+        // Step 2: Parsing an array of JSON objctes
+        Gson g2 = new Gson();
+        CatalogItem[] responseItems = g2.fromJson(response.toString(), CatalogItem[].class);
+        List<CatalogItem> catalogItems = new ArrayList();
+        for (CatalogItem item : responseItems) {
+            // Save response items in an array
+            catalogItems.add(item);
+        }
+
+        for (CatalogItem item : catalogItems) {
+            System.out.println(item.getCost());
+        }
+
+        
+
+    }
+
+    static String readJsonFromWebService(String url) throws MalformedURLException {
+        try {
+            // Web service URL
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            return response.toString();
+        } catch (Exception e) {
+            // TODO
+        }
+        return "";
     }
 
 }
